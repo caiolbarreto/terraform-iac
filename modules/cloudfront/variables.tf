ee9343c69dbaedcbd35f482ac1e03bd3deea7985
Caio Bezerra Londres Barreto <cbarreto@justworks.com>
2025-10-11 11:39:21 -0300

    create variables for s3 and cloudfront

--- modules/cloudfront/variables.tf
+++ modules/cloudfront/variables.tf
@@ -7,3 +7,15 @@ variable "bucket_domain_name" {
   type        = string
   description = "value of the bucket domain name"
 }
+
+variable "price_class" {
+  type        = string
+  default     = "PriceClass_200"
+  description = "value of the price class"
+}
+
+variable "cdn_tags" {
+  type        = map(string)
+  default     = {}
+  description = "value of the cdn tags"
+}
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -2,3 +2,9 @@ variable "s3_bucket_name" {
   type        = string
   description = "value of the bucket name"
 }
+
+variable "s3_tags" {
+  type        = map(string)
+  default     = {}
+  description = "value of the s3 tags"
+}
