f826d3e768e7721ca8b8c52eb6f7033fa35096ad
Caio Bezerra Londres Barreto <cbarreto@justworks.com>
2025-10-11 11:26:54 -0300

    create cloudfront with s3 data

---
+++ modules/cloudfront/variables.tf
@@ -0,0 +1,9 @@
+variable "origin_id" {
+  type        = string
+  description = "value of the origin id"
+}
+
+variable "bucket_domain_name" {
+  type        = string
+  description = "value of the bucket domain name"
+}
