4b5bd89135c4449f87090117ed882f981c1982ff
Caio Bezerra Londres Barreto <caiolondresb@gmail.com>
2025-10-13 20:59:32 -0300

    create state bucket

--- providers.tf
+++ providers.tf
@@ -5,9 +5,30 @@ terraform {
       version = "6.16.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "terraform-state-bucket-tf-caio"
+    key     = "terraform.tfstate"
+    region  = "sa-east-1"
+    profile = "caiobarreto"
+    encrypt = true
+  }
 }
 
 provider "aws" {
   region  = "sa-east-1"
   profile = "caiobarreto"
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Iac = true
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "state_bucket" {
+  type        = string
+  default     = "terraform-state-bucket-tf-caio"
+  description = "value of the state bucket"
+}
